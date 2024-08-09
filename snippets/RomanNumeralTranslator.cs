c56485e7968f79dd412e012291219caa0c2ef0d1
SweptThrone <im@sweptthr.one>
2024-08-09 14:28:36 -0500

    use StringBuilder instead of strings

--- snippets/RomanNumeralTranslator.cs
+++ snippets/RomanNumeralTranslator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 /*
 Name:  Roman Numeral Translator
@@ -12,28 +13,32 @@ namespace RomanNumeralTranslator {
     internal class Translator {
         // in:  integer to convert to Roman numerals
         // out:  Roman numeral as a string, or an empty string if the input is <= 0
-        static string numToRoman( int num ) {
+        static string NumToRoman( int num ) {
             int[] romanNumbers = new int[ 7 ] { 1000, 500, 100, 50, 10, 5, 1 };
             char[] romanNumerals = new char[ 7 ] { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
 
-            string outStr = "";
+            StringBuilder strB = new StringBuilder();
 
             while ( num > 0 ) {
                 for ( int i = 0; i < romanNumbers.Length; i++ ) {
                     if ( num >= romanNumbers[ i ] ) {
-                        outStr += romanNumerals[ i ];
+                        strB.Append( romanNumerals[ i ] );
                         num -= romanNumbers[ i ];
 
                         // if the last four chars are all the same, we're breaking the rules of Roman numerals,
                         // and we need to replace three of them with the next numeral up, however...
-                        if ( outStr.Length >= 4 && outStr[ outStr.Length - 1 ] == outStr[ outStr.Length - 2 ] && outStr[ outStr.Length - 2 ] == outStr[ outStr.Length - 3 ] && outStr[ outStr.Length - 3 ] == outStr[ outStr.Length - 4 ] && i - 1 >= 0 ) {
-                            outStr = outStr.Substring( 0, outStr.Length - 3 ) + romanNumerals[ i - 1 ];
+                        if ( strB.Length >= 4 && strB[ strB.Length - 1 ] == strB[ strB.Length - 2 ] && strB[ strB.Length - 2 ] == strB[ strB.Length - 3 ] && strB[ strB.Length - 3 ] == strB[ strB.Length - 4 ] && i - 1 >= 0 ) {
+                            strB.Remove( strB.Length - 3, 3 );
+                            strB.Append( romanNumerals[ i - 1 ] );
 
                             // ..if this results in a sandwich like VIV which is invalid,
                             // we need to replace that with the NEXT next numeral up
-                            if ( outStr.Length >= 3 && outStr[ outStr.Length - 1 ] == outStr[ outStr.Length - 3 ] ) {
-                                char btwn = outStr[ outStr.Length - 2 ];
-                                outStr = outStr.Substring( 0, outStr.Length - 3 ) + btwn + romanNumerals[ i - 2 ];
+                            if ( strB.Length >= 3 && strB[ strB.Length - 1 ] == strB[ strB.Length - 3 ] ) {
+                                char btwn = strB[ strB.Length - 2 ];
+
+                                strB.Remove( strB.Length - 3, 3 );
+                                strB.Append( btwn );
+                                strB.Append( romanNumerals[ i - 2 ] );
                             }
                         }
                         break;
@@ -41,7 +46,7 @@ namespace RomanNumeralTranslator {
                 } // end for
             } // end while
 
-            return outStr;
+            return strB.ToString();
         }
 
         static void Main( string[] args ) {
@@ -50,7 +55,7 @@ namespace RomanNumeralTranslator {
             while ( numberToTest >= 0 ) {
                 Console.Write( "> " );
                 if ( int.TryParse( Console.ReadLine(), out numberToTest ) ) {
-                    Console.WriteLine( "= " + numToRoman( numberToTest ) );
+                    Console.WriteLine( "= " + NumToRoman( numberToTest ) );
                 } else {
                     Console.WriteLine( "= Error on input." );
                 } // end if/else
